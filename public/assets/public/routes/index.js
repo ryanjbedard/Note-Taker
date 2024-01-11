bbcd6730e2d6d0a9bad9c96d70364c0782f061f2
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-01-10 22:33:31 -0800

    Add routing elements on server.js file.

---
+++ public/assets/public/routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+// Import our files containing our routes
+const notesRouter = require('./notes');
+
+router.use('/notes', notesRouter);
+
+module.exports = router;
